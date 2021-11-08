7d70b9d187f4a882f5357a038228501745c700e9
romank123 <67678390+romank123@users.noreply.github.com>
2021-11-08 21:37:24 +0300

    add mixitup.js

---
+++ app.js
@@ -0,0 +1,28 @@
+console.log('app.js')
+
+var btn = document.querySelectorAll('[data-filter]')
+
+;[...btn].map((e) => {
+  e.onmouseover = () => {
+    ;(e.style.backgroundColor = `var(--primary-action-color)`),
+      (e.style.color = 'white')
+  }
+  e.onmouseout = () => {
+    e.getAttribute('data') !== ''
+      ? ((e.style.backgroundColor = `var(--btn-category-color)`),
+        (e.style.color = `var(--project-item-heading-color)`))
+      : ''
+  }
+  e.onclick = () => {
+    ;[...btn].map((e) => {
+      e.classList.remove('active-btn'),
+        //e.setAttribute('data', 'active'),
+        (e.style.backgroundColor = ''),
+        (e.style.color = '')
+    })
+    e.setAttribute('data', ''),
+      e.classList.add('active-btn'),
+      (e.style.backgroundColor = `var(--primary-action-color)`),
+      (e.style.color = 'white')
+  }
+})
